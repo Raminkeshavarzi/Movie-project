654023ca76f0c6cb53ed66ee0496f05ac3e9874a
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-10-14 18:41:16 +0200

    Reset the configuration

---
+++ filmo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Components
+
+// CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
