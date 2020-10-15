3de5d5ce8cecf3e476a417cdbf3afa3841094ff4
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-10-15 18:01:21 +0200

    fethed data from TMDB through useEffect and async function

--- filmo/src/Component/Display/Row/Row.js
+++ filmo/src/Component/Display/Row/Row.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react'
+
 // Component
+import axios from '../../../Container/axios/axios'
 // CSS
-const  Row = ({title}) => {
+
+
+
+const  Row = ({title, fetchURL}) => {
     // State
     const [setMovies, setMoviesState] = useState([]);
 
     // SetState
     useEffect(() => {
-
-    }, [])
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            setMoviesState(request.data.results);
+            return request;
+        };
+        fetchData()
+    }, [fetchURL])
 
     return (
         <div>
