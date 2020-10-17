36efbc72ee1a33231b96ae3c39be8bf7ecda4b71
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-10-17 10:52:45 +0200

    added NavBar

---
+++ filmo/src/Component/Navigation/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+// CSS
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className="nav">
+            <h1>Filmo</h1>
+        </div>
+    )
+}
+
+export default NavBar
