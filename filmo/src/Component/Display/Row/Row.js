f765b99d8186c32be1aa2596defff2c3bca39a89
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-10-17 10:54:46 +0200

    added Row

--- filmo/src/Component/Display/Row/Row.js
+++ filmo/src/Component/Display/Row/Row.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
 // Component
-import axios from '../../../Container/axios/axios'
+import axios from '../../../Container/axios/axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
+
 
 // CSS
 import './Row.css'
@@ -10,6 +13,8 @@ import './Row.css'
 const  Row = ({title, fetchURL,isLargeRow}) => {
     // State
     const [setMovies, setMoviesState] = useState([]);
+    const [setTrailerUrl, setTrailerUrlState] = useState('')
+
 
     // SetState
     useEffect(() => {
@@ -21,21 +26,48 @@ const  Row = ({title, fetchURL,isLargeRow}) => {
         fetchData()
     }, [fetchURL]);
     
+    // YouTube opts 
+    const opts={
+        height: '390px',
+        width: '100%',
+        playerVars:{
+          
+          autoplay: 1,
+        }
+    }
     // Base URL
     const imgURL = 'https://image.tmdb.org/t/p/original/';
+
+    // Event Handler
+    const trailerEventHandler = (item) => {
+         if(setTrailerUrl){
+            setTrailerUrlState('');
+         } else{
+             movieTrailer(item.name || '')
+             .then(url =>{
+                 const urlParam = new URLSearchParams(new URL(url).search)
+                 setTrailerUrlState(urlParam.get('v'));
+                }).catch(error => {
+                 console.log(error)
+             })
+         }
+    }
 
     return (
         <div className="row">
             <h2>{title}</h2>
+            
             <div className="row__photos">
                 {setMovies.map(ele => (
-                    <img className="row__photo" 
+                    <img className={`row__photo ${isLargeRow && 'row__photoLarge'}`}
                         src={`${imgURL}${isLargeRow ?  ele.poster_path: ele.backdrop_path}`} 
                         alt={ele.name}
                         key={ele.id}
+                        onClick={() => trailerEventHandler(ele)}
                     />
                 ))}
             </div>
+            {setTrailerUrl && <YouTube videoId={setTrailerUrl} opts={opts} /> }
         </div>
     )
 }
